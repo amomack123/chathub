f646e16f506c3c31559347b99c664285d84226ae
Amogh <amogh.maheshwari@email.com>
2024-12-31 17:05:48 -0800

    feat(chat): add ChatContext and LLM integration for chat functionality but without too much ui. UI includes the sidebar newSession

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ChatProvider } from "@/context/chat/provider";
+import { Sidebar } from "@/components/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ChatProvider>{children}</ChatProvider>
+        <ChatProvider>
+          <div className="w-full h screen flex flex-row"> 
+            <Sidebar />
+            {children}
+            </div>
+            </ChatProvider>
       </body>
     </html>
   );
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { useChatContext } from "@/context/chat/context";
+import { Button } from "./ui/button";
 
 
 export const Sidebar = () => {
      const {sessions, createSession} = useChatContext()
      return (
         <div>
-            
+            <Button onClick = {() =>  createSession()}> 
+                New Session
+            </Button>
+            {sessions?.map((session) =>  (
+                <div key={session.id}>
+                    {session?.title}
+                </div>
+            ))}
         </div>
      )
 }
